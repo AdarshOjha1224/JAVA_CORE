b323372694282105f5b33ec71c6dcff72519f098
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-02-25 17:56:20 +0530

    learning about Object Oriented Programming in JAVA Language

---
+++ src/UpcastingAndDowncasting.java
@@ -0,0 +1,39 @@
+
+class Papa {
+    public void show1(){
+        System.out.println("Hello papa Ji !!");
+    }
+}
+
+class Beta extends Papa {
+    public void show2(){
+        System.out.println("Hello beta Ji !!");
+    }
+}
+
+
+public class UpcastingAndDowncasting {
+    public static void main(String[] args) {
+
+        // TYPECASTING ->>>>
+        double d = 1.5;
+        int i = (int)d;
+        System.out.println(i);
+
+
+        // UPCASTING ->>>>
+        Papa p1 = (Papa) new Beta(); // you dont have to mention it implicitly.
+        // it can also written as    [  Papa p1 = new Beta();  ]
+        // the object of Beta is refer to the Papa (So we are going up) ->>
+        p1.show1();
+
+
+        // DOWNCASTING ->>>>
+        // why we need this -> child-class can access the properties of parent-class,
+        // but parent-class cant access the properties of child-class.
+        // To Make possible this we use the concept of Downcasting.
+        Beta b1 = (Beta) p1 ;
+        b1.show2();
+
+    }
+}
