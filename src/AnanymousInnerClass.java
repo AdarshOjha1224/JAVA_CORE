3208fe011da0940964898d9b2137a54be4903360
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-02-26 09:05:23 +0530

    learning about Object Oriented Programming in JAVA Language

---
+++ src/AnanymousInnerClass.java
@@ -0,0 +1,30 @@
+
+class Animal {
+    public void show() {
+        System.out.println("This is Animal...");
+    }
+}
+
+// If you know that this extended class is going to use only once,
+// then why we create this ??
+
+//class Monkey extends Animal {
+//    public void show(){
+//        System.out.println("This is Monkey...");
+//    }
+//}
+
+
+public class AnanymousInnerClass {
+    public static void main(String[] args) {
+
+        // NEW IMPLEMENTATION -> (Inner class inside the main class, with NO NAME -> ANONYMOUS)
+        Animal an1 = new Animal() {
+            public void show() {
+                System.out.println("This is Monkey...");
+            }
+        };
+
+        an1.show(); // Here it will call the show of new implementation.
+    }
+}
---
+++ src/InnerClass.java
@@ -0,0 +1,56 @@
+// static is used only for the inner class.
+// you cant make outer class static.
+
+class _A {
+
+    int age ;
+
+    public void show(){
+        System.out.println("In the show !!");
+    }
+
+    class _B {
+        // It creates the class _A$_B
+
+        public void print(){
+
+            System.out.println("Printing.....");
+        }
+    }
+
+    static class _C {
+        public void display(){
+            System.out.println("This is Static class !!");
+        }
+    }
+}
+
+
+public class InnerClass {
+    public static void main(String[] args) {
+
+        _A a1 = new _A();
+        a1.show();
+
+//        _B is not openly available.
+//        _B b1 = new _B(); ->>> ERROR
+
+        _A._B b1 = a1.new _B();
+        //->> A.B means B belongs to A.
+        // ->> if you want to call show() , which is a non-static method , which means
+        // you need object to call it.
+        // ->> if you want to access age , then you need a object of _A.
+        // ->> Similarly, if you want to create a object of _B(to call print() which is non-static method) then,
+        // you need the object of _A.
+
+        b1.print();
+
+        _A._C c1 = new _A._C(); // only work when the class is static.
+
+        c1.display();
+
+        //
+
+
+    }
+}
