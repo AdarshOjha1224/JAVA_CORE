54454b85237f7fc54614ea75d49c1012f2246ac9
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-02-26 18:33:22 +0530

    learning about Object Oriented Programming in JAVA Language

---
+++ src/AbstractAndAnonymous.java
@@ -0,0 +1,26 @@
+
+abstract class Parent {
+    public abstract void show();
+    public abstract void display();
+}
+
+
+public class AbstractAndAnonymous {
+    public static void main(String[] args) {
+
+        // if you want to implement interface/abstract class only once then you can use inner class.
+        // we are creating the object not Parent , we are creating the object of inner class.
+        Parent p1 = new Parent() {
+            public void show() {
+                System.out.println("In the new show!!");
+            }
+
+            @Override
+            public void display() {
+                System.out.println("In the new Display");
+            }
+        };
+
+
+    }
+}
