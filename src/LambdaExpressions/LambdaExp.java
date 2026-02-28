bb6d67032e94ac04c90a9f2b46a124b0cd49407c
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-02-28 22:04:20 +0530

    learning about Object Oriented Programming in JAVA Language

---
+++ src/LambdaExpressions/LambdaExp.java
@@ -0,0 +1,29 @@
+package LambdaExpressions;
+
+@FunctionalInterface
+interface A {
+    void show(int i);
+}
+
+
+public class LambdaExp {
+    public static void main(String[] args) {
+
+//        A a1 = new A() {
+//            @Override
+//            public void show(int i) {
+//                System.out.println("In the show !! " + i);
+//            }
+//        };
+
+        /// LAMBDA EXPRESSION ->>>
+
+//        A a1 = (int i) -> System.out.println("In the show ! " + i);
+//        A a1 = (i) -> System.out.println("In the show ! " + i);
+
+        // create less class files , but the LamdaExp class got heavier.
+        A a1 = i -> System.out.println("In the show ! " + i);
+
+        a1.show(20);
+    }
+}
