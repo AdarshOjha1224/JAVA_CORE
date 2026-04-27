e80d4cd9e0cfbfee7e8b6095147a7712d0e92c78
Adarsh Ojha <adarshojha1224@gmail.com>
2026-04-27 23:58:53 +0530

    Meaning of Static word in java

---
+++ src/Static/Student.java
@@ -0,0 +1,33 @@
+package Static;
+
+public class Student {
+    public int count = 0 ;
+
+    private int id ;
+    private String name = "Adarsh" ;
+    public int age ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
