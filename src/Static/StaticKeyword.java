925bf93189e20b95675529eb664033ae9750e28c
Adarsh Ojha <adarshojha1224@gmail.com>
2026-04-28 23:35:02 +0530

    Meaning of Static word in java

--- src/Static/StaticKeyword.java
+++ src/Static/StaticKeyword.java
@@ -4,11 +4,12 @@ package Static;
 // It can be applied to variable , methods , blocks and nested classes.
 // The main concept behind static is that it belongs to the class rather than instance of the class.
 
-
 public class StaticKeyword {
     public static void main(String[] args) {
-        Student stud1 = new Student();
 
+
+
+        Student stud1 = new Student();
         System.out.println(stud1.getAge());
     }
 }
