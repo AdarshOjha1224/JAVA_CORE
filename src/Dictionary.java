0aa9b1f7197e06ca4227acc428a8c039b1078b36
Adarsh Ojha <adarshojha1224@gmail.com>
2026-05-07 23:52:38 +0530

    Creating simple dictionary program in java

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -6,17 +6,12 @@ public class Dictionary {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the count of values : ");
         int n = sc.nextInt();
-
         String[] word = new String[n];
-
         for(int i = 0 ; i < n ; i++){
             System.out.print("Enter the word no. " + (i+1) + " : ");
             word[i] = sc.next();
         }
-
-
         int[][] letter = new int[n][];
-
         for(int i = 0 ; i < n ; i++){
             int word_l = word[i].length();
             letter[i] = new int[word_l];
