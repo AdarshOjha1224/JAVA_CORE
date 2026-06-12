1df8da3163684648ded717ab1a1f72948ce0d935
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-06-12 23:02:30 +0530

    Linked List in JAVA

--- src/Linked_List/linkedList.java
+++ src/Linked_List/linkedList.java
@@ -8,18 +8,18 @@ import java.util.LinkedList;
 public class linkedList {
     public static void main(String[] args) {
         // Node -> Data + Reference.
-        LinkedList<Integer> lList = new LinkedList<>();
-        lList.add(10);
-        lList.add(20);
-        lList.add(30);
-        lList.get(2); // O(n)
-        lList.addLast(33); // O(1)
-        lList.addFirst(1); // O(1)
-        lList.getFirst();
-        lList.getLast();
-        System.out.println(lList);
-        lList.removeIf(x -> x%2==0);
-        System.out.println(lList);
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.get(2); // O(n)
+        list.addLast(33); // O(1)
+        list.addFirst(1); // O(1)
+        list.getFirst();
+        list.getLast();
+        System.out.println(list);
+        list.removeIf(x -> x%2==0);
+        System.out.println(list);
 
         LinkedList<String> animals = new LinkedList<>(Arrays.asList("Cat","Dog","Tiger","elephant"));
         LinkedList<String> animalsRemove = new LinkedList<>(Arrays.asList("Dog","Tiger"));
