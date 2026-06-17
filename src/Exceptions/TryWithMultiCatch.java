f6a2bb5fe80f17c54fbfd3bfb8d77dc7594b0d6e
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-06-17 14:03:01 +0530

    Try with multiple Catch. Order and behaviour of multiple catch holding different Exception.

--- src/Exceptions/TryWithMultiCatch.java
+++ src/Exceptions/TryWithMultiCatch.java
@@ -9,6 +9,9 @@ public class TryWithMultiCatch {
         int[] nums = new int[5];
         String str = null;
 
+///        Even if we have Multiple Catch for different type of exception in the try.
+///        After it provoke the first exception it will not run the complete try block and after the execution of catch block for corresponding exception it will continue the code.
+
         try {
             j = 18/i;
             System.out.println(str.length());
@@ -26,7 +29,7 @@ public class TryWithMultiCatch {
         }
 
 //        Exception class (parent) should be at the bottom of all the exceptions (child)
-
+//        If you make it at top then it'll create an issue.
 
         System.out.println(j);
         System.out.println("The end.");
