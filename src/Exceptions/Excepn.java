bb3186d3411ad7f580bff9a4803c93491eb86085
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-06-17 14:00:39 +0530

    Exception Handling with the help of try-catch. Behaviour of Exception.

--- src/Exceptions/Excepn.java
+++ src/Exceptions/Excepn.java
@@ -15,8 +15,11 @@ public class Excepn {
 
 //        // critical
 //        int a = 0 ;
-//        int b = 10/a ;
-//        // After the exception , the code stops running.
+//        int b = 10/a;
+
+//        When exception arise , the code stops running.
+//        To Make it run without any halt , Exception Handling Required.
+
 //        System.out.println(b);
 
         int a = 0 ;
@@ -26,16 +29,22 @@ public class Excepn {
 //        you can write normal statements in the try as well , but it is more sensible to write only critical.
 //        Catch block runs only when the Exception occurs, otherwise the catch block is skipped.
 //        Exception is class and e is a object here.
+
+        // Hey JAVA execute this code -
+        // Run try block
+        // If runs without any error catch block will be skipped continue OR
+        // If not then run catch block then continue.
+
         try {
             b = 10/a;
         }
+        // its not working , Java throw an error and you have to catch it.
+        // it is throwing an Object.
         catch (Exception e) {
             System.out.println("SOMETHING WENT WRONG..." + e);
         }
 
-
         System.out.println(b);
-
         System.out.println("BYE BYE");
 
     }
