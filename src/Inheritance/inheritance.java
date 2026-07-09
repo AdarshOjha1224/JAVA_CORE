5cd466ad51e130ea3e7989356e0ed444e009c791
adarsh <adarshojha1224@gmail.com>
2026-07-09 23:55:09 +0530

    Implementation of Inheritance in Java.

--- src/Inheritance/inheritance.java
+++ src/Inheritance/inheritance.java
@@ -37,6 +37,7 @@ public class inheritance {
         String r7 = c1.musicSystem(false);
         String r8 = c1.headlight(true);
         System.out.println(r7 + "\n" + r8);
+
         String r9 = c1.backCamera(true);
         String r10 = c1.airCondition(false);
         System.out.println(r9 + "\n" + r10);
