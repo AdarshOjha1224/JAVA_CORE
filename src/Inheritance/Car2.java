69ffadbfddedb45f8d7002c751ed668023d88a50
adarsh <adarshojha1224@gmail.com>
2026-07-10 23:54:30 +0530

    Implementation of Inheritance in Java.

--- src/Inheritance/Car2.java
+++ src/Inheritance/Car2.java
@@ -1,12 +1,10 @@
 package Inheritance;
 
 class Car2 extends Car1 {
-
     public String headlight(boolean head){
         if(head) return "Head light is on";
         return "head light is off";
     }
-
     public String musicSystem(boolean music){
         if(music) return "Music System is on";
         return "Music System is off";
